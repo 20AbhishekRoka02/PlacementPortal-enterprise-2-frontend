6722d216e5301304face56a10db5dcd0a8254c3f
20AbhishekRoka02 <75659036+20AbhishekRoka02@users.noreply.github.com>
2026-07-21 07:29:29 +0530

    Fix fetch profile api call

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
         const cookieStore = await cookies();
 
         const response = await fetch(
-            `${process.env.BACKEND_URL}/student/profile`,
+            `${process.env.BACKEND_URL}/student/profile/`,
             {
                 headers: {
                     Cookie: cookieStore.toString(),
